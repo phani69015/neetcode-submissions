f9d03e9b3751723559c0013a6b9380e49d99dc95
Tirumala Phanendra <125112335+phani69015@users.noreply.github.com>
2026-07-26 11:56:50 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        d = {}
+        def dfs(day,holding):
+            if day >= len(prices):
+                return 0
+            if (day,holding) in d:
+                return d[(day,holding)]
+            if holding:
+                sell = prices[day] + dfs(day+2, False)
+                keep = dfs(day+1,True)
+                ans = max(sell,keep)
+            else:
+                buy = -prices[day] + dfs(day+1,True)
+                skip = dfs(day+1,False)
+                ans = max(buy,skip)
+            d[(day,holding)]= ans
+            return ans
+
+        return dfs(0,False)  
+        
